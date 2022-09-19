b32543187ce56480b4f4b12cc0662fe7be559167
alen devassy <alendevassy09@gmail.com>
2022-09-19 16:19:09 +0530

    todo app crud create and delete added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  let date = new Date()
+let day = date.getDate();
+let month = date.getMonth()+1;
+let year = date.getFullYear();
+
+let fullDate = `${day}.${month}.${year}.`;
+console.log(fullDate);
+  
+  let [toDos,setTodos]=useState([])
+  let [toDo,setTodo]=useState('')
   return (
     <div className="app">
       <div className="mainHeading">
@@ -9,22 +19,47 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>{fullDate}</h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e)=>{setTodo(e.target.value)}} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>{if(toDo!==''){setTodos([...toDos,{id:Date.now(),text:toDo,status:false}])}}} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
+        {
+          toDos.map((obj)=>{
+            return(
+            <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <input onChange={(e)=>{
+              console.log(e.target.checked);
+              console.log(obj);
+
+              setTodos(toDos.map((obj2)=>{
+                if(obj2.id===obj.id){
+                 obj2.status=e.target.checked
+                }
+                return obj2
+              }))
+            }} value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
-            <i className="fas fa-times"></i>
+            <i onClick={()=>{
+              setTodos(toDos.filter((obj3)=>{
+                if(obj3.id!==obj.id){
+                  return obj3
+                }
+                return null
+              }))
+            }} className="fas fa-times"></i>
           </div>
+          
         </div>
+        
+          )})
+        
+         }
       </div>
     </div>
   );
